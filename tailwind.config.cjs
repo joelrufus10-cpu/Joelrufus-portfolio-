bafa0ab052cc29f1b4c7390ee24e67088a73471c
joelrufus10-cpu <joelrufus10@gmail.com>
2025-10-07 16:06:21 +0100

    Add files via upload

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neon-pink': '#ff2d88',
+        'neon-blue': '#6ee7ff',
+        'bg-dark': '#07070a',
+      },
+    },
   },
   plugins: [],
 }
